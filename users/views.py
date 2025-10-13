d3c5f0c8857aba88cd78fef64b13620c301a38ff
Pranjalitupat <pranjalitupat04@gmail.com>
2025-10-13 22:20:18 +0530

    UserRegistrationForm

---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully for {username}!')
+            return redirect('login')  # redirect to login page after registration
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
